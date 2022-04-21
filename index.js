2c20ba09757b52be4299712a9eb752afefdbe411
rutherford-alexis <rutherford.aalexis@gmail.com>
2022-04-20 23:51:24 -0400

    done

--- index.js
+++ index.js
@@ -15,27 +15,24 @@
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-class Person {
-  constructor(name, age) {
-    this.name = name;
-    this.age = age;
-    this.stomach = [];
-  }
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
 }
-Person.prototype.eat()= function(edible){
+Person.prototype.eat = function(edible){
   if (this.stomach.length < 10){ 
-    this.stomach.push(edible);
-  };
+    this.stomach.push(edible)
+  }
  };
 
-Person.prototype.poop()= function(){
+Person.prototype.poop= function(){
    this.stomach = [];
  }
 
-Person.prototype.toString()= ()=>{
+Person.prototype.toString = function(){
    return `${this.name}, ${this.age}`;
  }
-
 
 /*
   TASK 2
@@ -53,10 +50,22 @@ Person.prototype.toString()= ()=>{
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
 }
-
+Car.prototype.fill= function(gallons) {
+  this.tank = this.tank + gallons;
+};
+Car.prototype.drive= (distance) => {
+  this.odometer = this.odometer + distance;
+  this.tank = this.tank - (distance/ this.milesPerGallon)
+  if (this.tank === 0){
+    distance = 0;
+  }
+}
 
 /*
   TASK 3
@@ -66,17 +75,23 @@ function Car() {
         + Should return a string "Playing with x", x being the favorite toy.
 */
 
-function Baby() {
-
+function Baby(name, age, favoriteToy) {
+  this.name= name;
+  this.age= age;
+  this.favoriteToy= favoriteToy;
+}
+Baby.prototype = Object.create(Person.prototype);
+Baby.prototype.play= function(){
+  return `Playing with ${this.favoriteToy}`;
 }
 
 
 /* 
   TASK 4
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
+  1. window binding, "this" will become the console in object form
+  2. implicit binding, "this" takes place of the object 
+  3. new binding, you can use "this".(a key) = (a value) in a fuction to create an object
   4. 
 */
 
