5e1f4748170a288a0b9d8cf102666aaa1e635cfe
rutherford-alexis <rutherford.aalexis@gmail.com>
2022-04-20 21:25:05 -0400

    idk

--- index.js
+++ index.js
@@ -15,9 +15,26 @@
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
+  }
 }
+Person.prototype.eat()= function(edible){
+  if (this.stomach.length < 10){ 
+    this.stomach.push(edible);
+  };
+ };
+
+Person.prototype.poop()= function(){
+   this.stomach = [];
+ }
+
+Person.prototype.toString()= ()=>{
+   return `${this.name}, ${this.age}`;
+ }
 
 
 /*
